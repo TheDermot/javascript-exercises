219149d48cf17e051851d547bef374c6e775c95d
TheDermot <88741983+TheDermot@users.noreply.github.com>
2025-01-25 01:20:53 -0500

    Pushing progress to Github
    Working on each problem just need it online for remote

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  let convertedToC = (fahrenheit - 32) * (5 / 9);
+  return Math.round(convertedToC * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  let convertedToF = celsius * (9 / 5) + 32;
+  return Math.round(convertedToF * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
